18250bc554b3be4ff515c5a49afb3bf4c59483e5
Nicholas Pipitone <npip99@gmail.com>
2020-08-10 08:10:13 +0000

    Pass in version number to vanilla setup script

--- vanilla-setup.sh
+++ vanilla-setup.sh
@@ -1,19 +1,54 @@
+#!/bin/bash
+
+SERVER_PATH=$HOME/minecraft_server
+
+print_usage () {
+    echo "Usage: $0 [-v \$VERSION] [-d \$SERVER_DIRECTORY]"
+}
+
+while getopts "hv:d:" opt; do
+  case ${opt} in
+    v )
+      VERSION=${OPTARG}
+      ;;
+    d )
+      SERVER_PATH=$OPTARG
+      if [ -z $SERVER_PATH ]; then
+          echo "Server directory -d must not be an empty string!"
+      fi
+      ;;
+    h )
+      print_usage
+      exit 0
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z $VERSION ]; then
+    echo "Version number must be provided!"
+    print_usage
+    exit 1
+fi
+
 sudo apt install update
 
 sudo apt install wget openjdk-8-jdk screen -y
 
-filedir=$HOME/minecraft_server
-
-if [ -e $filedir ]; then
+if [ -e $SERVER_PATH ]; then
     echo
-    echo "$filedir exists! Please delete it first!"
-    exit
+    echo "$SERVER_PATH exists! Please delete it first!"
+    exit 1
 fi
 
-mkdir $filedir
-cp run.sh $filedir
-cd $filedir
-wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar
+mkdir $SERVER_PATH
+cp run.sh $SERVER_PATH
+cd $SERVER_PATH
+jar_url=$(curl https://mcversions.net/download/$VERSION | sed -r 's/^.*Server Jar[^\:]*\:\/\/([^\"]*).*$/http:\/\/\1/g')
+wget -O minecraft_server.jar $jar_url
 chmod +x minecraft_server.jar
 echo eula=true >> eula.txt
 
